34e7f4272020b683c22d49fb5bae5e262cac43b3
Avshi <avshalomsh@mta.ac.il>
2025-03-11 11:32:42 +0200

    hi

--- Waste_recognition/Main.py
+++ Waste_recognition/Main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:eed9979879be4d18c16286d9439a2fc7d8744bb887d78bd2987e7fbadeb84a57
-size 20
+oid sha256:2b837c759ff6d340b4b79f75d8bca470b86be5d0b42f6ebcb2ad9292199ddf78
+size 1306
