ecce4b73d68bbb712fc645dcd3a4bd1e9be92bbf
Avshi <avshalomsh@mta.ac.il>
2025-03-12 12:19:07 +0200

    uplaod new function that gets a photo and classify it

--- Waste_recognition/Main.py
+++ Waste_recognition/Main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2b837c759ff6d340b4b79f75d8bca470b86be5d0b42f6ebcb2ad9292199ddf78
-size 1306
+oid sha256:5284b69af9df21007411a3ffe3b930866f14988a188df3fcb583b6c9c563303c
+size 1835
--- Waste_recognition/train_waste_model.py
+++ Waste_recognition/train_waste_model.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0e1bc030aeb9c028ebdb714369a42e4dbea6c495998cfe84f065aa2888680f5e
-size 2112
+oid sha256:2812a6139afcbee947d1e8571cf80b98ec16932f09a57afe282b4d2f9a085be5
+size 2168
