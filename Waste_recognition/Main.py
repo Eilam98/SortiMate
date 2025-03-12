c313c13b929ffcb18624a223000b7d7959f4ca22
Avshi <avshalomsh@mta.ac.il>
2025-03-12 08:45:04 +0200

    h

--- Waste_recognition/Main.py
+++ Waste_recognition/Main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2b837c759ff6d340b4b79f75d8bca470b86be5d0b42f6ebcb2ad9292199ddf78
-size 1306
+oid sha256:3c78609b3093d41486db91cbd826772997da5be0422ab6bf3625816eff1368ed
+size 2150
--- Waste_recognition/train_waste_model.py
+++ Waste_recognition/train_waste_model.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5ae88412be625cd967c59e9dde96b20059e364730ff6c4f87a6ec1c3f53a8145
-size 2182
+oid sha256:aabf0eae8f0fe62ac3b4b8d507a0ac6cf5e6e8781e31a9f7a61492f1aab1f57d
+size 2163
