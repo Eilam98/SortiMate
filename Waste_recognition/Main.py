2a713d8cc5a654a4a41b06a5a77e1a71b59dbd7f
Avshi <avshalomsh@mta.ac.il>
2025-03-26 19:07:56 +0200

    changing the type of the main, which is right now recognize the an image through path in the C: disk.

--- Waste_recognition/Main.py
+++ Waste_recognition/Main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fe65ccae0450afd3063c0306cde8ee267529e881255ac5d378cd14a3453df41e
-size 2980
+oid sha256:60f623bfee0ee533e3b272e28555db6343b93e14fb7f9c732beb28eb75ac4066
+size 1716
