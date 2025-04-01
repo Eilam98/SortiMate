d79855a9c7a48a45ce1c2d3f2edc436d24bcccc9
Avshi <avshalomsh@mta.ac.il>
2025-04-01 14:50:23 +0300

    Ok, right now I have succeeded in merging the two functions. By pressing the 'c' key, we can take a pic and classify the product!

--- Waste_recognition/Classifier.py
+++ Waste_recognition/Classifier.py
@@ -1,23 +1,31 @@
 import gradio as gr
+import numpy as np
 from transformers import AutoImageProcessor
 from transformers import SiglipForImageClassification
 from transformers.image_utils import load_image
 from PIL import Image
 import torch
+import cv2
 
 # Load model and processor
 model_name = "prithivMLmods/Augmented-Waste-Classifier-SigLIP2"
 model = SiglipForImageClassification.from_pretrained(model_name)
 processor = AutoImageProcessor.from_pretrained(model_name)
 
+
 # test_image = "C://Users//user//Desktop//Project_SortiMate//SortiMate//Waste_recognition//dataset//glass//glass11.jpg"
-test_image = "C://Users//user//Downloads//prod3.jpg"
+# test_image = "C://Users//user//Downloads//prod3.jpg"
 
 
-def waste_classification(image=test_image):
+def waste_classification(image):
     """Predicts waste classification for an image."""
-    image = Image.open(image).convert("RGB")
-    # image = Image.fromarray(image).convert("RGB")
+
+    # If the input is a numpy array, convert it from BGR (OpenCV) to RGB and wrap it as a PIL image.
+    if isinstance(image, np.ndarray):
+        image = Image.fromarray(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+    else:
+        image = Image.open(image).convert("RGB")
+
     inputs = processor(images=image, return_tensors="pt")
 
     with torch.no_grad():
@@ -34,20 +42,19 @@ def waste_classification(image=test_image):
 
     return predictions
 
-
 # Create Gradio interface
-iface = gr.Interface(
-    fn=waste_classification,
-    inputs=gr.Image(type="numpy"),
-    outputs=gr.Label(label="Prediction Scores"),
-    title="Augmented Waste Classification",
-    description="Upload an image to classify the type of waste."
-)
+# iface = gr.Interface(
+#    fn=waste_classification,
+#    inputs=gr.Image(type="numpy"),
+#    outputs=gr.Label(label="Prediction Scores"),
+#    title="Augmented Waste Classification",
+#    description="Upload an image to classify the type of waste."
+# )
 
 # Launch the app
-if __name__ == "__main__":
-    waste = waste_classification()
-    print("The probabilities are: ", waste)
-    waste = max(waste, key=waste.get)
-    print("The waste is :", waste)
-    # iface.launch()
+# if __name__ == "__main__":
+#    waste = waste_classification()
+#    print("The probabilities are: ", waste)
+#    waste = max(waste, key=waste.get)
+#    print("The waste is :", waste)
+# iface.launch()
