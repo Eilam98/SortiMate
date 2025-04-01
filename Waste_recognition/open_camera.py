dd63c899650bf8a6e33bf6396c25feff106bddf5
Avshi <avshalomsh@mta.ac.il>
2025-04-01 14:50:23 +0300

    Ok, right now I have succeeded in merging the two functions. By pressing the 'c' key, we can take a pic and classify the product!

--- Waste_recognition/open_camera.py
+++ Waste_recognition/open_camera.py
@@ -1,17 +1,29 @@
 import cv2
-
-
+from Classifier import waste_classification
 
 cap = cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
-ESC_KEY = 27
-while True:
-    success, frame = cap.read()
-    if success:
-        cv2.imshow("capture image", frame)
-        if cv2.waitKey(1) == ESC_KEY:  # esc button is being pressed to quit filming
-            break
+print("Press 'c' to capture an image for classification, or 'ESC' to exit.")
 
+ESC_KEY = 27
+
+while True:
+    ret, frame = cap.read()
+    if ret:
+        cv2.imshow("Camera", frame)
+        key = cv2.waitKey(1) & 0xFF
+
+        if key == ESC_KEY:
+            break
+        elif key == ord('c'):
+            frame_to_classify = frame.copy()
+            predictions = waste_classification(frame_to_classify)
+            predicted_label = max(predictions, key=predictions.get)
+
+            print("Predicted waste type: ", predicted_label)
+            print("predicted score: ", predictions[predicted_label])
+
+cap.release()
 cv2.destroyAllWindows()
