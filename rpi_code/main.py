d47c92e7fb806301565587056adc27b9203ab3f5
Smart Bin RPI <SortiMateRPI@example.com>
2025-04-16 15:18:44 +0300

    angles changes for servo motor

--- rpi_code/main.py
+++ rpi_code/main.py
@@ -5,11 +5,16 @@ def main():
     try:
         sorter = SortingMechanism(rotation_pin=17, gate_pin=27) # GPIO17 for rotation servo, GPIO27 for gate servo
         print("Smart Recycling Bin initialized...")
-
-        sorter.gate_servo.set_angle(90)
+        
         time.sleep(2)
-        sorter.gate_servo.set_angle(0)
+        sorter.gate_servo.set_angle(270)
         time.sleep(2)
+        sorter.gate_servo.set_angle(180)
+        time.sleep(2)
+        sorter.gate_servo.set_angle(120)
+        time.sleep(2)
+        sorter.gate_servo.set_angle(30)
+        
         
     except KeyboardInterrupt:
         print("\nProgram interrupted by user")
