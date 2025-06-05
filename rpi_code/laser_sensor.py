79fcb10d849ea0565b2e17b1c99823b6ff1a95d7
Rotem Waingart <Rotem.waingart@gmail.com>
2025-06-05 16:06:36 +0300

    increased treshhold of the laser sensor to 250

--- rpi_code/laser_sensor.py
+++ rpi_code/laser_sensor.py
@@ -6,7 +6,7 @@ import atexit
 NUM_BEAM_BROKEN_CHECKS = 10 
 PASS_CHECKS_PRECENTAGE = 0.8  # 80% of checks must be broken to consider beam broken
 class LaserSensor:
-    def __init__(self, laser_pin=23, threshold=100):
+    def __init__(self, laser_pin=23, threshold=250):
         """
         Initialize the laser sensor with specified pins and threshold.
         
