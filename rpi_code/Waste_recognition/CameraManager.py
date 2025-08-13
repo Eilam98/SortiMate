5141d0cf581809554b8e4dd19b29682175388e93
Rotem Waingart <Rotem.waingart@gmail.com>
2025-08-13 10:57:46 +0300

    implement camera quality and cropping improvments

--- rpi_code/Waste_recognition/CameraManager.py
+++ rpi_code/Waste_recognition/CameraManager.py
@@ -10,26 +10,24 @@ class CameraManager:
         # Initialize and configure for still capture
         self.picam2 = Picamera2()
         self.picam2.configure(self.picam2.create_still_configuration(
-            main={"format": "RGB888", "size": (640, 480)} # TO EDIT: size and format
+            main={"format": "RGB888", "size": (3280, 2464)}
         ))
+        self.picam2.set_controls({"ScalerCrop": (820, 616, 1640, 1232)})
         self.picam2.start()
         
-        #TO DELETE?
         base_dir = os.path.dirname(os.path.abspath(__file__))
         self.images_dir = os.path.join(base_dir, "temporary_images")
         os.makedirs(self.images_dir, exist_ok=True)
 
     def capture_image(self):
-        time.sleep(0.2)  # Let the camera adjust exposure
-        frame_bgr = self.picam2.capture_array("main") # Grab the frame – it comes out BGR
-        frame_rgb = frame_bgr[..., ::-1] # Convert BGR ➜ RGB (swap channels 0↔2)
+        time.sleep(0.2)  
+        frame_bgr = self.picam2.capture_array("main") 
+        frame_rgb = frame_bgr[..., ::-1]
 
-        # TO DELETE?
         im = Image.fromarray(frame_rgb)
         im.save(os.path.join(self.images_dir, "current_image.jpg"))
 
         return frame_rgb
 
     def __del__(self):
-        # Stop the camera preview before exiting
         self.picam2.stop_preview()
